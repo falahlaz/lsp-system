bbc90db6d1f880806fa9721be4001a5852473c00
Falahlaz <alfalahlazuardi@gmail.com>
2020-08-14 15:59:16 +0700

    Created for form apl 01

---
+++ app/Form01Scheme.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Form01Scheme extends Model
+{
+    protected $table = 't_scheme_form01';
+
+    protected $guarded = [];
+}
