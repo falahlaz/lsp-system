d5d6e8673d808b30c1fa9833c8743c2a8ee9d519
arisdanang <arisdanang2810@gmail.com>
2020-09-03 11:55:47 +0700

    commit

--- resources/views/admin/Element/index.blade.php
+++ resources/views/admin/Element/index.blade.php
@@ -2,76 +2,76 @@
 @section('title', 'Elemen')
 @section('sub-judul','Elemen')
 @section('content')
-   <div class="col-12 col-md-5 col-lg-5">
-      <div class="card">
-         <div class="card-header">
-            <h4>Add New Elemen</h4>
-         </div>
-         <div class="card-body">
-            <div class="form-group">
-               <label>Nama Elemen</label>
-               <input type="text" class="form-control"/>
+    <div class="col-12 col-md-5 col-lg-5">
+        <div class="card">
+            <div class="card-header">
+                <h4>Add New Elemen</h4>
             </div>
-         </div>
-         <div class="card-footer text-right">
-            <button class="btn btn-primary mr-1" type="submit">Submit</button>
-         </div>
-      </div>
-   </div>
-   <div class="col-12 col-md-7 col-lg-7">
-      <div class="card">
-         <div class="card-header">
-            <h4>List of Elemen</h4>
-         </div>
-         <div class="card-body">
-            <div class="table-responsive">
-               <table class="table table-striped" id="table-1">
-                  <thead>
-                     <tr>
-                        <th class="text-center">
-                           #
-                        </th>
-                        <th>Nama Elemen</th>
-                        <th width="20%">Action</th>
-                     </tr>
-                  </thead>
-                  <tbody>
-                     <tr>
-                        <td>1</td>
-                        <td>Klaster Perawatan Pencegahan (Preventive Maintenance) Alat Berat Small Bulldozer</td>
-                        <td>
-                           <Link to="/elemen/detail" class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></Link>
-                           <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
-                        </td>
-                     </tr>
-                     <tr>
-                        <td>2</td>
-                        <td>Klaster Perawatan Pencegahan (Preventive Maintenance) Alat Berat Small Bulldozer</td>
-                        <td>
-                           <Link to="/elemen/detail"  class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></Link>
-                           <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
-                        </td>
-                     </tr>
-                     <tr>
-                        <td>3</td>
-                        <td>Klaster Perawatan Pencegahan (Preventive Maintenance) Alat Berat Small Bulldozer</td>
-                        <td>
-                           <Link to="/elemen/detail" class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></Link>
-                           <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
-                        </td>
-                     </tr>
-                     <tr>
-                        <td>4</td>
-                        <td>Klaster Perawatan Pencegahan (Preventive Maintenance) Alat Berat Small Bulldozer</td>
-                        <td>
-                           <Link to="/elemen/detail" class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></Link>
-                           <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
-                        </td>
-                     </tr>
-                  </tbody>
-               </table>
+            <div class="card-body">
+                <div class="form-group">
+                <label>Nama Elemen</label>
+                <input type="text" class="form-control"/>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
+            <div class="card-footer text-right">
+                <button class="btn btn-primary mr-1" type="submit">Submit</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-md-7 col-lg-7">
+        <div class="card">
+            <div class="card-header">
+                <h4>List of Elemen</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                <table class="table table-striped" id="table-1">
+                    <thead>
+                        <tr>
+                            <th class="text-center">
+                            #
+                            </th>
+                            <th>Nama Elemen</th>
+                            <th width="20%">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Klaster Perawatan Pencegahan (Preventive Maintenance) Alat Berat Small Bulldozer</td>
+                            <td>
+                            <Link to="/elemen/detail" class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></Link>
+                            <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Klaster Perawatan Pencegahan (Preventive Maintenance) Alat Berat Small Bulldozer</td>
+                            <td>
+                            <Link to="/elemen/detail"  class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></Link>
+                            <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Klaster Perawatan Pencegahan (Preventive Maintenance) Alat Berat Small Bulldozer</td>
+                            <td>
+                            <Link to="/elemen/detail" class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></Link>
+                            <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>Klaster Perawatan Pencegahan (Preventive Maintenance) Alat Berat Small Bulldozer</td>
+                            <td>
+                            <Link to="/elemen/detail" class="btn btn-icon btn-info"><i class="fas fa-info-circle"></i></Link>
+                            <a href="#" class="btn btn-icon btn-danger"><i class="fas fa-times"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
