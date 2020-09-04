a427cb53941d5cd48ecec4511a0bf96a59a7ad80
arisdanang <arisdanang2810@gmail.com>
2020-09-04 11:39:13 +0700

    coba commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('user.login');
+});
+
 Route::get('/',function(){
     return view('user.form02');
 });
