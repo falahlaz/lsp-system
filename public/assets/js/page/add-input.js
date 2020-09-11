14d25e4331f83202bfbf7a12e4bdd9ae68130591
alfalahlazuardi <alfalahlazuardi@gmail.com>
2020-09-11 20:48:29 +0700

    Fixing additional input for element

--- public/assets/js/page/add-input.js
+++ public/assets/js/page/add-input.js
@@ -1,6 +1,6 @@
 $('.add-input').click(function() {
     $('.form-add-input').append(`<div class="input-group colorpickerinput mt-3">
-                            <input type="text" class="form-control">
+                            <input type="text" name="name[]" class="form-control">
                             <div class="input-group-append">
                               <div class="input-group-text btn btn-danger" style="cursor: pointer" onclick="deleteInput(this)">
                                 <i class="fas fa-times"></i>
