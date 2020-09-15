ad76a5d5969fd1e53f1d9b8143e5358eb8224f69
arisdanang <arisdanang2810@gmail.com>
2020-09-16 00:56:39 +0700

    controller untuk halaman profile user

---
+++ app/Http/Controllers/Admin/Master/ProfileController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Master;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class ProfileController extends Controller
+{
+    public function index(){
+        $name  = \DB::table('m_asesor')->select('name')->get();
+        return view('admin.app');
+    }
+
+    public function show($id)
+    {
+        $data = Assessor::find($id);
+
+        return view('admin.profile.index',\compact('data'));
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request,[
+            'name'=> 'required',
+            'email' => 'required',
+            'phone' => 'required',
+        ]);
+
+        User::where('id',$id)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone
+        ]);
+
+        Assessor::where('id',$id)->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone
+        ]);
+
+        return redirect()->route('admin.profile');
+    }
+}
