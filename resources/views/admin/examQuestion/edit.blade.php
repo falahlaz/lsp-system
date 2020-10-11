6ce1b1e202da586f23a6e68c76440c94fbf1514c
ashandileo <ashandileonadi2001@gmail.com>
2020-10-11 21:49:00 +0700

    fix button exam question

--- resources/views/admin/examQuestion/edit.blade.php
+++ resources/views/admin/examQuestion/edit.blade.php
@@ -30,7 +30,7 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="text-right">
+                        <div class="text-right pb-4 pr-4">
                             <a href="{{ route('admin.exam.question.index') }}" class="btn btn-outline-danger">Back</a>
                             <button class="btn btn-primary" type="submit">Update</button>
                         </div>
